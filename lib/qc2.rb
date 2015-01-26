c57082490778f4f2b9b752689814c628b50e452f
Alexander Boyd <alex@opengroove.org>
2015-01-25 23:25:43 -0700

    Tracing experiments
    Committing here because tracing some code I've got that makes HTTP
    requests is causing what appear to be some :b_return events without
    corresponding :b_call events. Specifically:
    
    INVALID POP: Expected #<struct QCMethodCall class_name="Net::BufferedIO", method_name=:rbuf_fill, type=:method> but got #<struct QCMethodCall class_name="Net::BufferedIO", method_name=:rbuf_fill, type=:block>
    INVALID POP: Expected #<struct QCMethodCall class_name="Net::HTTPResponse", method_name=:inflater, type=:method> but got #<struct QCMethodCall class_name="Net::HTTPResponse", method_name=:inflater, type=:block>
    INVALID POP: Expected #<struct QCMethodCall class_name="Net::HTTP", method_name=:start, type=:method> but got #<struct QCMethodCall class_name="Net::HTTP", method_name=:start, type=:block>
    STATS: [16509, 16512]
    
    These ought to be looked into and corresponding bugs against Ruby opened.

---
+++ lib/qc2.rb
@@ -0,0 +1,63 @@
+
+QCMethodCall = Struct.new(:class_name, :method_name, :type) do
+  attr_reader :events
+
+  def initialize(*args)
+    super
+    @events = []
+  end
+
+  def immediately_equal?(other)
+    return other.is_a?(QCMethodCall) && other.class_name == self.class_name && other.method_name == self.method_name && other.type == self.type
+  end
+end
+
+QCEvent = Struct.new(:file, :line) do
+  def immediately_equal?(other)
+    return other.is_a?(Event) && other.file == self.file && other.line == self.line
+  end
+end
+
+def trace_and_track(&block)
+  stack2 = [QCMethodCall.new('toplevel', :toplevel, :toplevel)]
+  stats = [0, 0]
+  begin
+    trace_point = TracePoint.new(:line, :call, :return, :c_call, :c_return, :b_call, :b_return) do |tp|
+      begin
+        if tp.event == :c_call || tp.event == :call || tp.event == :b_call
+          stats[0] = stats[0] + 1
+          call = QCMethodCall.new(tp.defined_class.inspect, tp.method_id, tp.event == :b_call ? :block : :method)
+          stack2[-1].events.push(call) if stack2[-1]
+          stack2.push(call)
+        elsif tp.event == :c_return || tp.event == :return || tp.event == :b_return
+          stats[1] = stats[1] + 1
+          p = stack2[-1]
+          real = QCMethodCall.new(tp.defined_class.inspect, tp.method_id, tp.event == :b_return ? :block : :method)
+          unless real.immediately_equal?(p)
+            puts "INVALID POP: Expected #{p} but got #{real}"
+          else
+            stack2.pop
+          end
+        elsif tp.event == :line
+          stack2[-1].events.push(QCEvent.new(tp.path, tp.lineno)) if stack2[-1]
+        end
+      rescue => e
+        puts "Exception: #{e}"
+      end
+    end
+    trace_point.enable &block
+  ensure
+    trace_point.disable
+  end
+  puts "STATS: #{stats}"
+  stack2[0]
+end
+
+def trace2(&block)
+  r1 = trace_and_track(&block)
+  r2 = trace_and_track(&block)
+  if r1 != r2
+    raise 'Fragile test'
+  end
+  r1
+end
